679c93dde565d560e6fea95963aa7d8b56f2df84
judethadeus123 <wanyonyijude@gmail.com>
2023-03-17 10:45:19 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printf function
+ * return: 0 (Success)
+ */
+
+int main(void)
+{
+ptintf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
